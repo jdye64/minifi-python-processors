5de719404d0367243d0eeed6c5a9bececf8a0961
Jeremy Dyer <jeremydyer@apache.org>
2019-03-06 16:56:42 -0500

    Add processor to show message on SenseHAT LCD based on sensehat_message attribute received on incoming flow_file

---
+++ RPI/SenseHat/RPISenseHatShowMessage.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import codecs
+from sense_hat import SenseHat
+
+sense = SenseHat()
+
+def describe(processor):
+    processor.setDescription("Shows a message on the Raspberry PI SenseHat")
+
+def onInitialize(processor):
+    processor.setSupportsDynamicProperties()
+    sense.clear()
+
+class RPISenseHat(object):
+    def __init__(self):
+        self.content = None
+
+    def process(self, input_stream):
+        self.content = codecs.getreader('utf-8')(input_stream).read()
+        return len(self.content)
+
+def onTrigger(context, session):
+    flow_file = session.get()
+
+    if flow_file is None:
+        return
+
+    message = flow_file.getAttribute("sensehat_message")
+    sense.show_message(message)
+    session.transfer(flow_file, REL_SUCCESS)
